a8b1ca83e180d9bd7d6c5ca75b2886d13024e8b9
basit011 <156387950+basit011@users.noreply.github.com>
2025-03-24 10:26:08 -0400

    Initial Commit

---
+++ A2_iOS_ basit_101451227/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_ basit_101451227
+//
+//  Created by Basit Ali on 2025-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
