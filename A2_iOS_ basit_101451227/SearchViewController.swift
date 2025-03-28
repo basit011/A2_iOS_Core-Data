fa6007e2cd53fbbe568654b47cec08467f794d2b
basit011 <156387950+basit011@users.noreply.github.com>
2025-03-28 13:54:19 -0400

    Added label and logic to be able to click for search

--- A2_iOS_ basit_101451227/SearchViewController.swift
+++ A2_iOS_ basit_101451227/SearchViewController.swift
@@ -13,67 +13,95 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var selectedProductLabel: UILabel!
 
-    
-    var products = [Product]()
-    var filteredProducts = [Product]()
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    
-    
-        searchBar.delegate = self
-        tableView.delegate = self
-        tableView.dataSource = self
+        var products = [Product]()
+        var filteredProducts = [Product]()
+
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            searchBar.delegate = self
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ProductCell")
+            
+            // Initially hide the table view
+            tableView.isHidden = true
+            
+            // Load products from Core Data
+            loadProducts()
+        }
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ProductCell")
+        func loadProducts(with request: NSFetchRequest<Product> = Product.fetchRequest()) {
+            do {
+                products = try context.fetch(request)
+                filteredProducts = products
+                tableView.reloadData()
+            } catch let error {
+                print("Error loading products: \(error.localizedDescription)") // More detailed error message
+            }
+        }
         
-        // Load products from Core Data
-        loadProducts()
-    }
-    
-    
-    func loadProducts(with request: NSFetchRequest<Product> = Product.fetchRequest()) {
-        do {
-            products = try context.fetch(request)
-            filteredProducts = products
+        // MARK: - UISearchBar Delegate
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            // Always show the table view when the user starts typing
+            tableView.isHidden = false
+            
+            if searchText.isEmpty {
+                filteredProducts = products // Show all products if search is empty
+            } else {
+                // Apply search filter with multiple words
+                let request: NSFetchRequest<Product> = Product.fetchRequest()
+                request.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchText)
+                loadProducts(with: request)
+            }
+            
+            // Reload the table view after filtering
             tableView.reloadData()
-        } catch let error {
-            print("Error loading products: \(error.localizedDescription)") // More detailed error message
         }
-    }
 
-    
-    // MARK: - UISearchBar Delegate
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        // Filter products based on search text
-        if searchText.isEmpty {
-            filteredProducts = products
-        } else {
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            // Dismiss the keyboard when the "Enter" key is pressed
+            searchBar.resignFirstResponder()
+            
+            // Optionally, update UI or perform other actions (e.g., load details)
+            print("Search button pressed with text: \(searchBar.text ?? "")")
+            
+            // You can also call loadProducts to update the table view with filtered results
             let request: NSFetchRequest<Product> = Product.fetchRequest()
-            request.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchText) 
-            loadProducts(with: request)
+            if let searchText = searchBar.text, !searchText.isEmpty {
+                request.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchText)
+                loadProducts(with: request)
+            } else {
+                // If search text is empty, reload all products
+                loadProducts()
+            }
         }
-        tableView.reloadData()
-    }
-    
-    // MARK: - UITableView Data Source & Delegate
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredProducts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
-        let product = filteredProducts[indexPath.row]
         
-        // Assuming the Product entity has a 'name' attribute
-        cell.textLabel?.text = product.name
+        // MARK: - UITableView Data Source & Delegate
         
-        return cell
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return filteredProducts.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+            let product = filteredProducts[indexPath.row]
+            cell.textLabel?.text = product.name
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let selectedProduct = filteredProducts[indexPath.row]
+            
+            selectedProductLabel.text = "Selected Product: \(selectedProduct.name ?? "No name")"
+            
+            // Deselect the row after selection
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
-}
